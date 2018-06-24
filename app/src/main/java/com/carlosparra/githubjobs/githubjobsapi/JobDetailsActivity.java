b9d5db7e75a0434b8bd0a4b12a6cc47894fb5cd0
Carlos Parra <carlos.parra.sbr@gmail.cm>
2018-06-24 11:52:12 -0400

    Initial local commit

---
+++ app/src/main/java/com/carlosparra/githubjobs/githubjobsapi/JobDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.carlosparra.githubjobs.githubjobsapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JobDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_details);
+    }
+}
